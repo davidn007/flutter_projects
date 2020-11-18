5dd83184c9ba989abf3c892d86199d04a5aed26c
davidn <davidn4333@gmail.com>
2020-11-18 19:16:02 +0100

    finished up the app by during some finishing styling and added a display image to the README file

--- micard/README.md
+++ micard/README.md
@@ -1,6 +1,8 @@
 # Micard 
 ## A Single screen Personal Business Card App
 
+<img src="micard.png" width="350">
+
 Anyone can download the application and have all your details on their phone. It's a **Digital Business Card**
 
 _an image of the app will be added, once the application is completed (shouldn't take long)_
--- micard/lib/main.dart
+++ micard/lib/main.dart
@@ -11,8 +11,8 @@ class MiCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
       home: Scaffold(
+        backgroundColor: Colors.blueGrey[900],
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -23,11 +23,20 @@ class MiCard extends StatelessWidget {
               ),
               Text(
                 'David Nwaneri',
-                style: TextStyle(fontSize: 40.0),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontFamily: 'Pacifico',
+                ),
               ),
               Text(
                 'FLUTTER DEVELOPER',
-                style: TextStyle(fontSize: 20.0, color: Color(0xFF00F0F8)),
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.blueGrey[400],
+                  letterSpacing: 2.5,
+                ),
               ),
               SizedBox(
                 width: 150.0,
@@ -37,32 +46,30 @@ class MiCard extends StatelessWidget {
                 ),
               ),
               Card(
-                color: Color(0xFFFFFFFF),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
                     Icons.phone,
-                    color: Colors.teal,
+                    color: Colors.blueGrey[900],
                   ),
                   title: Text(
                     '+234 808 288 6274',
                     style:
-                        TextStyle(fontSize: 20.0, color: Colors.teal.shade900),
+                        TextStyle(fontSize: 20.0, color: Colors.blueGrey[600]),
                   ),
                 ),
               ),
               Card(
-                color: Color(0xFFFFFFFF),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
                 child: ListTile(
                   leading: Icon(
                     Icons.mail,
-                    color: Colors.teal,
+                    color: Colors.blueGrey[900],
                   ),
                   title: Text(
                     'davidn4333@gmail.com',
                     style:
-                        TextStyle(fontSize: 20.0, color: Colors.teal.shade900),
+                        TextStyle(fontSize: 20.0, color: Colors.blueGrey[600]),
                   ),
                 ),
               ),
